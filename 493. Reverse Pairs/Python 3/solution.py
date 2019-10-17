4e0df6df9b2065b758c5e616b6177e3846b24b54
FengruLi <fli35@dxc.com>
2019-10-17 13:58:55 -0500

    update

--- 493. Reverse Pairs/Python 3/solution.py
+++ 493. Reverse Pairs/Python 3/solution.py
@@ -1,16 +1,29 @@
+# class Solution:
+#     def reversePairs(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
+
+#         result = 0
+
+#         sorted_nums = sorted(nums)
+
+#         for idx, num in enumerate(nums):
+#             new_idx = bisect.bisect_left(sorted_nums, num)
+#             sorted_nums.pop(new_idx)
+#             i = bisect.bisect_left(sorted_nums, (num + 1) // 2)
+#             result += i
+
+#         return result
+
+
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
+        temp = []
         result = 0
 
-        sorted_nums = sorted(nums)
-
-        for idx, num in enumerate(nums):
-            new_idx = bisect.bisect_left(sorted_nums, num)
-            sorted_nums.pop(new_idx)
-            i = bisect.bisect_left(sorted_nums, (num + 1) // 2)
-            result += i
+        for num in nums:
+            idx = bisect.bisect_right(temp, 2 * num)
+            result += len(temp) - idx
+            bisect.insort(temp, num)
 
         return result
